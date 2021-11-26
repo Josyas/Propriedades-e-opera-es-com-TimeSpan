0963cd9d54f27b916dc09eeeb6bbc5a53d23ef2f
Josias <87546094+Josyas@users.noreply.github.com>
2021-11-25 23:11:18 -0300

    arquivos

---
+++ Propriedades e operações com TimeSpan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Propriedades_e_operações_com_TimeSpan {
+    class Program {
+        static void Main(string[] args) {
+
+            TimeSpan t = new TimeSpan(2, 3, 5, 7, 11);
+
+            Console.WriteLine(t);
+
+            Console.WriteLine("Days: " + t.Days);
+            Console.WriteLine("Hours: " + t.Hours);
+            Console.WriteLine("Minutes: " + t.Minutes);
+            Console.WriteLine("Milliseconds: " + t.Milliseconds);
+            Console.WriteLine("Seconds: " + t.Seconds);
+            Console.WriteLine("Ticks: " + t.Ticks);
+
+            Console.WriteLine("TotalDays: " + t.TotalDays);
+            Console.WriteLine("TotalHours: " + t.TotalHours);
+            Console.WriteLine("TotalMinutes: " + t.TotalMinutes);
+            Console.WriteLine("TotalSeconds: " + t.TotalSeconds);
+            Console.WriteLine("TotalMilliSeconds: " + t.TotalMilliseconds);
+        }
+    }
+}
